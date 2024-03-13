3beeb149ff143f0ed754cc70b5d13d3a438750bd
ArturoLuna7 <arturo712luna@gmail.com>
2024-03-12 17:33:26 -0700

    Aplicación de Singleton al Controlador

--- ProyectoFinal/src/main/java/com/example/proyectofinal/controlador/ControlJuego.java
+++ ProyectoFinal/src/main/java/com/example/proyectofinal/controlador/ControlJuego.java
@@ -3,7 +3,20 @@ package com.example.proyectofinal.controlador;
 import com.example.proyectofinal.modelo.ModeloJuego;
 
 public class ControlJuego {
+
+    private static ControlJuego instanciaControlJuego;
     private ModeloJuego modeloJuego;
+
+    private ControlJuego(){
+        modeloJuego = modeloJuego.getInstance();
+    }
+
+    public static synchronized ControlJuego getInstance(){
+        if (instanciaControlJuego == null){
+            instanciaControlJuego = new ControlJuego();
+        }
+        return instanciaControlJuego;
+    }
 
     public void dividirGrupo(){};
     public void terminarTurno(){};
