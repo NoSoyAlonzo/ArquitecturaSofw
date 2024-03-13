a163f41050c44241fe2b5a3506dba4cc6362e5ef
ArturoLuna7 <arturo712luna@gmail.com>
2024-03-12 17:33:48 -0700

    Aplicación de singleton a dominio

--- ProyectoFinal/src/main/java/com/example/proyectofinal/dominio/Tablero.java
+++ ProyectoFinal/src/main/java/com/example/proyectofinal/dominio/Tablero.java
@@ -4,10 +4,21 @@ import java.util.ArrayList;
 import java.util.List;
 
 public class Tablero {
-     private List<Grupo> listaGrupos = new ArrayList<>();
 
+    private static Tablero instanciaTablero;
+     private List<Grupo> listaGrupos;
+
+     private Tablero(){
+        listaGrupos = new ArrayList<>();
+     }
 //metodosP
 
+    public static synchronized Tablero getInstance(){
+         if (instanciaTablero == null){
+             instanciaTablero = new Tablero();
+         }
+         return instanciaTablero;
+    }
     public void obtenerGrupoDividido(){
 
     }
