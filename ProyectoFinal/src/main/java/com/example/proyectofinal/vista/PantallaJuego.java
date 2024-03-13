6c0d351ce6832120e080d7d4e0386d5a9de034e3
ArturoLuna7 <arturo712luna@gmail.com>
2024-03-12 17:34:12 -0700

    Aplicación de singleton a vista

--- ProyectoFinal/src/main/java/com/example/proyectofinal/vista/PantallaJuego.java
+++ ProyectoFinal/src/main/java/com/example/proyectofinal/vista/PantallaJuego.java
@@ -3,7 +3,20 @@ package com.example.proyectofinal.vista;
 import com.example.proyectofinal.controlador.ControlJuego;
 
 public class PantallaJuego {
+    private static PantallaJuego instanciaPantallaJuego;
     private ControlJuego controlJuego;
+
+    private PantallaJuego(){
+        controlJuego = ControlJuego.getInstance();
+    }
+
+    public static synchronized PantallaJuego getInstance(){
+        if (instanciaPantallaJuego == null){
+            instanciaPantallaJuego = new PantallaJuego();
+        }
+        return instanciaPantallaJuego;
+    }
+
     public void notificarCambios(){};
     public void clickPasarTurno(){};
     public void clickJalarFicha(){};
