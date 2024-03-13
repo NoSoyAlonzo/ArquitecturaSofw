66b6c727121cc1865b8c69ad77e97f30cfa2ecf0
ArturoLuna7 <arturo712luna@gmail.com>
2024-03-12 17:33:40 -0700

    Aplicación de singleton a dominio

--- ProyectoFinal/src/main/java/com/example/proyectofinal/dominio/Juego.java
+++ ProyectoFinal/src/main/java/com/example/proyectofinal/dominio/Juego.java
@@ -4,9 +4,22 @@ import java.util.ArrayList;
 import java.util.List;
 
 public class Juego {
-    private Pozo pozo;
-    private List<Jugador> listaJugador = new ArrayList<>();
 
+    private static Juego instanciaJuego;
+    private Pozo pozo;
+    private List<Jugador> listaJugador;
+
+    private Juego(){
+        pozo = Pozo.getInstance();
+        listaJugador = new ArrayList<>();
+    }
+
+    public static synchronized Juego getInstance(){
+        if (instanciaJuego == null){
+            instanciaJuego = new Juego();
+        }
+        return instanciaJuego;
+    }
     public void cambiarTurno(){};
     public void obtenerTurnoSiguiente(){};
     public void jalarFichasPozo(){};
