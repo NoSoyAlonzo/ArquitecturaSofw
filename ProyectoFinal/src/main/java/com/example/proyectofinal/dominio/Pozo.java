191c8733400d7febdf55a414ee5a215959c00037
ArturoLuna7 <arturo712luna@gmail.com>
2024-03-13 19:33:18 -0700

    commit

--- ProyectoFinal/src/main/java/com/example/proyectofinal/dominio/Pozo.java
+++ ProyectoFinal/src/main/java/com/example/proyectofinal/dominio/Pozo.java
@@ -2,15 +2,22 @@ package com.example.proyectofinal.dominio;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Random;
 
 public class Pozo {
 
     private static Pozo instanciaPozo;
+    private Juego juego;
+    Excepcion exc;
     private List<Ficha> listaFichas;
 
     private Pozo(){
         listaFichas = new ArrayList <Ficha>();
+        exc = new Excepcion();
+        juego = Juego.getInstance();
     }
+
+
     //metodos
     public static synchronized Pozo getInstance(){
         if (instanciaPozo == null){
@@ -19,8 +26,24 @@ public class Pozo {
         return instanciaPozo;
     }
     public void quitarFicha(){}
-    public void validarPozoVacio(){}
+    public void validarPozoVacio(){
+        if (listaFichas == null || listaFichas.isEmpty()) {
+
+            return;
+        }else{
+            juego.pasarFichaJugador(quitarFichaAleatoria());
+        }
+    }
     public void jalarFicha(){};
+
+    public Ficha quitarFichaAleatoria() {
+        Ficha fichaObtenida;
+        Random random = new Random();
+        int indiceAleatorio = random.nextInt(listaFichas.size());
+        fichaObtenida = listaFichas.get(indiceAleatorio);
+        listaFichas.remove(indiceAleatorio);
+        return fichaObtenida;
+    }
 
 
 }
