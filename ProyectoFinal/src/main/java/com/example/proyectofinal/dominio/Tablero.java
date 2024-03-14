1786afc669b34c6ce57f35aed16b8ffc5b368fbd
ArturoLuna7 <arturo712luna@gmail.com>
2024-03-13 22:12:16 -0700

    Instancia juego, obtenerGruposModificados y validar grupo aplicados

--- ProyectoFinal/src/main/java/com/example/proyectofinal/dominio/Tablero.java
+++ ProyectoFinal/src/main/java/com/example/proyectofinal/dominio/Tablero.java
@@ -6,12 +6,13 @@ import java.util.List;
 public class Tablero {
 
     private static Tablero instanciaTablero;
+    private Juego juego;
      private List<Grupo> listaGrupos;
 
      private Tablero(){
         listaGrupos = new ArrayList<>();
+        juego = Juego.getInstance();
      }
-//metodosP
 
     public static synchronized Tablero getInstance(){
          if (instanciaTablero == null){
@@ -23,6 +24,31 @@ public class Tablero {
 
     }
     public void obtenerGruposModificados(){
+        List<Grupo> gruposModificados = new ArrayList<>();
+        for (int i = 0; i < listaGrupos.size(); i++) {
+            if (listaGrupos.get(i).isModificado()){
+                gruposModificados.add(listaGrupos.get(i));
+            }
+        }
 
+        if (validarGrupo(gruposModificados).isEmpty()) {
+            juego.cambiarTurno();
+        }
     }
+
+    /**
+     * Método que permite validar los grupos modificados, retorna una lista vacía
+     * si todos los grupos estaban según las reglas de negocio.
+     * @param gruposModificados
+     * @return
+     */
+    public List<Grupo> validarGrupo(List<Grupo> gruposModificados){
+        for (int i = 0; i < gruposModificados.size(); i++) {
+            if (gruposModificados.get(i).validarGrupo()){
+                gruposModificados.remove(gruposModificados.get(i));
+            }
+        }
+        return gruposModificados;
+    }
+
 }
