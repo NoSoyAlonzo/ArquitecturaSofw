5a94558d04f2cb0c69f7f29e9e276a33883216f4
ArturoLuna7 <arturo712luna@gmail.com>
2024-03-13 21:29:28 -0700

    Excepción para errores creada

--- ProyectoFinal/src/main/java/com/example/proyectofinal/dominio/Excepcion.java
+++ ProyectoFinal/src/main/java/com/example/proyectofinal/dominio/Excepcion.java
@@ -7,7 +7,7 @@ public class Excepcion {
     public Excepcion() {
     }
 
-    private void mostrarExcepcion(String titulo, String mensaje) {
+    public void mostrarExcepcion(String titulo, String mensaje) {
         Alert alerta = new Alert(Alert.AlertType.ERROR);
         alerta.setTitle(titulo);
         alerta.setHeaderText(null);
