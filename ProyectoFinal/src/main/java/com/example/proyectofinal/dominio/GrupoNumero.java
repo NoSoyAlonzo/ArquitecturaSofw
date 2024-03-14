b54e27166959ce7b7c276f091d23fb5689bbbac0
ArturoLuna7 <arturo712luna@gmail.com>
2024-03-13 19:33:05 -0700

    commit

--- ProyectoFinal/src/main/java/com/example/proyectofinal/dominio/GrupoNumero.java
+++ ProyectoFinal/src/main/java/com/example/proyectofinal/dominio/GrupoNumero.java
@@ -1,4 +1,12 @@
 package com.example.proyectofinal.dominio;
 
+import java.util.List;
+
 public class GrupoNumero extends Grupo{
+    public GrupoNumero(List<Ficha> listaFichas, boolean modificado) {
+        super(listaFichas, modificado);
+    }
+
+    public GrupoNumero() {
+    }
 }
