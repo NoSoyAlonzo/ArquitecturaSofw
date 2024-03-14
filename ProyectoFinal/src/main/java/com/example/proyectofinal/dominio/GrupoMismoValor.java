a309cff0584302a58976e997d80e4b7319e2f8f9
ArturoLuna7 <arturo712luna@gmail.com>
2024-03-13 19:33:00 -0700

    commit

--- ProyectoFinal/src/main/java/com/example/proyectofinal/dominio/GrupoMismoValor.java
+++ ProyectoFinal/src/main/java/com/example/proyectofinal/dominio/GrupoMismoValor.java
@@ -1,4 +1,14 @@
 package com.example.proyectofinal.dominio;
 
+import java.util.List;
+
 public class GrupoMismoValor extends Grupo{
+    public GrupoMismoValor(List<Ficha> listaFichas, boolean modificado) {
+        super(listaFichas, modificado);
+    }
+
+    public GrupoMismoValor() {
+    }
+
+
 }
