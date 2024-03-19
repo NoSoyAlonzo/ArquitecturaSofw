e16aa5d00575b507a258b36ae688cc942797a8e0
ArturoLuna7 <arturo712luna@gmail.com>
2024-03-18 22:19:40 -0700

    comentarios

--- ProyectoFinal/src/main/java/com/example/proyectofinal/dominio/FichaComodin.java
+++ ProyectoFinal/src/main/java/com/example/proyectofinal/dominio/FichaComodin.java
@@ -1,13 +1,25 @@
 package com.example.proyectofinal.dominio;
 
 public class FichaComodin extends Ficha{
+    /**
+     * Atributos de la clase
+     */
     private String simbolo;
 
+    /**
+     * Constructor con parámetros
+     * @param color
+     * @param simbolo
+     */
     public FichaComodin(ColorManager color, String simbolo) {
         super(color);
         this.simbolo = simbolo;
     }
 
+    /**
+     * Métodos get y set
+     * @return
+     */
     public String getSimbolo() {
         return simbolo;
     }
