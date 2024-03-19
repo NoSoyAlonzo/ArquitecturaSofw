25f7820c6aba6622c345a66922d12767e57f8984
ArturoLuna7 <arturo712luna@gmail.com>
2024-03-18 22:19:50 -0700

    comentarios

--- ProyectoFinal/src/main/java/com/example/proyectofinal/dominio/GrupoMismoValor.java
+++ ProyectoFinal/src/main/java/com/example/proyectofinal/dominio/GrupoMismoValor.java
@@ -3,10 +3,18 @@ package com.example.proyectofinal.dominio;
 import java.util.List;
 
 public class GrupoMismoValor extends Grupo{
+    /**
+     * Constructor de la clase
+     * @param listaFichas
+     * @param modificado
+     */
     public GrupoMismoValor(List<Ficha> listaFichas, boolean modificado) {
         super(listaFichas, modificado);
     }
 
+    /**
+     * Constructor vacio
+     */
     public GrupoMismoValor() {
     }
 
