f947163bfdcdf4fce5a19f78c75554993e440b46
ArturoLuna7 <arturo712luna@gmail.com>
2024-03-18 22:19:55 -0700

    comentarios

--- ProyectoFinal/src/main/java/com/example/proyectofinal/dominio/GrupoNumero.java
+++ ProyectoFinal/src/main/java/com/example/proyectofinal/dominio/GrupoNumero.java
@@ -3,10 +3,18 @@ package com.example.proyectofinal.dominio;
 import java.util.List;
 
 public class GrupoNumero extends Grupo{
+    /**
+     * Constructor con parámetros
+     * @param listaFichas
+     * @param modificado
+     */
     public GrupoNumero(List<Ficha> listaFichas, boolean modificado) {
         super(listaFichas, modificado);
     }
 
+    /**
+     * Constructor vacio
+     */
     public GrupoNumero() {
     }
 }
