8ae59580fe0069fadd64f7dfdd2f46d03a6ec8de
ArturoLuna7 <arturo712luna@gmail.com>
2024-03-18 22:19:34 -0700

    comentarios

--- ProyectoFinal/src/main/java/com/example/proyectofinal/dominio/Excepcion.java
+++ ProyectoFinal/src/main/java/com/example/proyectofinal/dominio/Excepcion.java
@@ -4,9 +4,17 @@ import javafx.scene.control.Alert;
 
 public class Excepcion {
 
+    /**
+     * Constructor de la clase
+     */
     public Excepcion() {
     }
 
+    /**
+     * Método que muestra un mensaje
+     * @param titulo
+     * @param mensaje
+     */
     public void mostrarExcepcion(String titulo, String mensaje) {
         Alert alerta = new Alert(Alert.AlertType.ERROR);
         alerta.setTitle(titulo);
