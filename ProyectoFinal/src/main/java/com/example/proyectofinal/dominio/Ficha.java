46944fd130be80b6dedeac5b08f1e563e0c70f31
ArturoLuna7 <arturo712luna@gmail.com>
2024-03-18 22:13:00 -0700

    Comentarios clase Ficha

--- ProyectoFinal/src/main/java/com/example/proyectofinal/dominio/Ficha.java
+++ ProyectoFinal/src/main/java/com/example/proyectofinal/dominio/Ficha.java
@@ -4,12 +4,26 @@ import javafx.scene.paint.Color;
 
 public class Ficha {
 
+    /**
+     * Atributos de la clase
+     * Ficha
+     */
     private ColorManager color;
 
+    /**
+     * Método constructor de una ficha
+     * @param color
+     */
     public Ficha(ColorManager color) {
         this.color = color;
     }
 
+    /**
+     * Gets y sets de
+     * los atributos de
+     * una ficha
+     * @return
+     */
     public ColorManager getColor() {
         return color;
     }
