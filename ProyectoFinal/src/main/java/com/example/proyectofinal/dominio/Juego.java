dda16b18a70fb0171017c79fe420924d11ff76db
ArturoLuna7 <arturo712luna@gmail.com>
2024-03-18 22:13:34 -0700

    Se agregaron comentarios y métodos

--- ProyectoFinal/src/main/java/com/example/proyectofinal/dominio/Juego.java
+++ ProyectoFinal/src/main/java/com/example/proyectofinal/dominio/Juego.java
@@ -6,53 +6,36 @@ import java.util.Random;
 
 public class Juego {
 
+    /**
+     * Atributos de la clase Juego
+     */
     private static Juego instanciaJuego;
     private Pozo pozo;
     private List<Jugador> listaJugador;
     private Ficha[] fichasCompletas;
     private Jugador jugadorTurno;
 
-
+    /**
+     * Método constructor de la clase
+     * Juego con el singleton aplicado
+     */
     private Juego(){
         pozo = Pozo.getInstance();
         listaJugador = new ArrayList<>();
         fichasCompletas =  new Ficha[106];
     }
 
-    public static synchronized Juego getInstance(){
-        if (instanciaJuego == null){
-            instanciaJuego = new Juego();
-        }
-        return instanciaJuego;
+    /**
+     * Métodos get y set
+     * @return
+     */
+    public Jugador getJugadorTurno() {
+        return jugadorTurno;
     }
 
-    public Jugador cambiarTurno(){
-        Random random = new Random();
-        int indiceAleatorio = random.nextInt(listaJugador.size());
-        return jugadorTurno = listaJugador.get(indiceAleatorio);
+    public void setJugadorTurno(Jugador jugadorTurno) {
+        this.jugadorTurno = jugadorTurno;
     }
-
-    public void obtenerTurnoSiguiente(){
-        cambiarTurno();
-    };
-
-    public void jalarFichaPozo(){
-        pozo.validarPozoVacio();
-    };
-
-    public void pasarFichaJugador(Ficha ficha){
-        if (ficha != null) {
-            for (int i = 0; i < listaJugador.size(); i++) {
-                if (listaJugador.get(i).getNombreUsuario().equalsIgnoreCase(jugadorTurno.getNombreUsuario())) {
-                    listaJugador.get(i).agregarFicha(ficha);
-                }
-            }
-            cambiarTurno();
-        }else{
-            throw new IllegalStateException("Objeto ficha vacío");
-        }
-    };
-
     public Pozo getPozo() {
         return pozo;
     }
@@ -68,4 +51,59 @@ public class Juego {
     public void setListaJugador(List<Jugador> listaJugador) {
         this.listaJugador = listaJugador;
     }
+
+
+    /**
+     * Mpetodo que permite obtener una instancia de Juego
+     * @return
+     */
+    public static synchronized Juego getInstance(){
+        if (instanciaJuego == null){
+            instanciaJuego = new Juego();
+        }
+        return instanciaJuego;
+    }
+
+    /**
+     * Método para cambiar el turno del juego.
+     * @return
+     */
+    public Jugador cambiarTurno(){
+        Random random = new Random();
+        int indiceAleatorio = random.nextInt(listaJugador.size());
+        return jugadorTurno = listaJugador.get(indiceAleatorio);
+    }
+
+    /**
+     * Método para obtener el turno siguiente.
+     */
+    public void obtenerTurnoSiguiente(){
+        cambiarTurno();
+    };
+
+    /**
+     * Método para jalar una ficha del pozo
+     */
+    public void jalarFichaPozo(){
+        pozo.validarPozoVacio();
+    };
+
+    /**
+     * Método para agregar una ficha a un jugador
+     * @param ficha
+     */
+    public void pasarFichaJugador(Ficha ficha){
+        if (ficha != null) {
+            for (int i = 0; i < listaJugador.size(); i++) {
+                if (listaJugador.get(i).getNombreUsuario().equalsIgnoreCase(jugadorTurno.getNombreUsuario())) {
+                    listaJugador.get(i).agregarFicha(ficha);
+                }
+            }
+            cambiarTurno();
+        }else{
+            throw new IllegalStateException("Objeto ficha vacío");
+        }
+    };
+
+
 }
