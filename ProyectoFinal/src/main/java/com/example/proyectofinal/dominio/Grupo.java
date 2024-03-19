72d6b0ab2e99d5f8ecfe73081dbc8e38e9b08b98
ArturoLuna7 <arturo712luna@gmail.com>
2024-03-18 22:13:30 -0700

    Se agregaron comentarios y métodos

--- ProyectoFinal/src/main/java/com/example/proyectofinal/dominio/Grupo.java
+++ ProyectoFinal/src/main/java/com/example/proyectofinal/dominio/Grupo.java
@@ -4,18 +4,36 @@ import java.util.ArrayList;
 import java.util.List;
 import com.example.proyectofinal.dominio.Ficha;
 public class Grupo {
+    /**
+     * Atributos de la clase Grupo
+     */
     private List<Ficha> listaFichas;
     private boolean modificado;
 
+
+    /**
+     * Método constructor de un grupo
+     * con parámetros
+     * @param listaFichas
+     * @param modificado
+     */
     public Grupo(List<Ficha> listaFichas, boolean modificado) {
         this.listaFichas = listaFichas;
         this.modificado = modificado;
     }
 
+    /**
+     * Método constructor de un grupo
+     * sin parámetros
+     */
     public Grupo() {
         listaFichas =new ArrayList<Ficha>();
     }
 
+    /**
+     * Métodos get y set para los atributos
+     * @return
+     */
     public List<Ficha> getListaFichas() {
         return listaFichas;
     }
@@ -32,10 +50,11 @@ public class Grupo {
         this.modificado = modificado;
     }
 
-    //metodos
-    public void verificarColocarFicha(){
-
-    }
+    /**
+     * Método que permite agregar una ficha
+     * a la lista de fichas de un grupo
+     * @param ficha
+     */
     public void agregarFicha(Ficha ficha){
         if (ficha != null) {
             listaFichas.add(ficha);
@@ -44,41 +63,69 @@ public class Grupo {
         }
     }
 
+    /**
+     * Método que permite validar los
+     * movimientos que se realizan a un grupo.
+     */
     public void validarMovimientosGrupos(){
 
     }
 
-    public boolean dividirGrupo(Grupo grupoDividir, List<Ficha> listaFichasSeleccionadas){
+    /**
+     * Método que permite dividir un grupo
+     * al recibir el grupo a dividir y la lista de fichas
+     * seleccionadas a dividir.
+     * Busca fichas, quita fichas y retorna un nuevo grupo para ser
+     * guardado en el tablero.
+     * @param grupoDividir
+     * @param listaFichasSeleccionadas
+     * @return
+     */
+    public Grupo dividirGrupo(Grupo grupoDividir, List<Ficha> listaFichasSeleccionadas){
+        Grupo grupoNuevo = new Grupo();
         boolean fichasRemovidas = false;
         for (int i = 0; i < listaFichasSeleccionadas.size(); i++) {
             if (grupoDividir.getListaFichas().contains(listaFichasSeleccionadas.get(i))){
-                listaFichas.remove(listaFichasSeleccionadas.get(i));
-                System.out.println("se removió: "+listaFichasSeleccionadas.get(i).getNumero());
+                grupoDividir.getListaFichas().remove(listaFichasSeleccionadas.get(i));
                 fichasRemovidas = true;
+                grupoDividir.setModificado(true);
             }
         }
         if (fichasRemovidas == true) {
-            agregarGrupo(grupo);
+            for (int i = 0; i < listaFichasSeleccionadas.size(); i++) {
+                grupoNuevo.agregarFicha(listaFichasSeleccionadas.get(i));
+            }
+            return grupoNuevo;
         }else{
             throw new IllegalStateException("No se encontraron las fichas");
         }
     }
 
-    public void dividir(List<Ficha> listaFicha, Grupo grupo){
-        boolean fichasRemovidas = false;
-        for (int i = 0; i < listaFicha.size(); i++) {
-            if (grupo.getListaFichas().contains(listaFicha.get(i))){
-                listaFicha.remove(listaFicha.get(i));
-                System.out.println("se removió: "+listaFicha.get(i).getNumero());
-                fichasRemovidas = true;
+    /**
+     * Método que permite verificar el colocar una ficha en un grupo
+     */
+    public void verificarColocarFicha(Grupo grupoModificar, List<Ficha> fichasColocar, Juego juegoJugador){
+        // Se verifica que se puedan poner las fichas en el grupo
+        if (verificarPonerFicha(grupoModificar, fichasColocar)) {
+            //Quitar fichas a jugador
+            juegoJugador.getJugadorTurno().quitarFicha(fichasColocar);
+
+        }else{
+            throw new IllegalStateException("No se pudo colocar la ficha");
+        }
+    }
+
+
+    public boolean verificarPonerFicha(Grupo grupoModificar, List<Ficha> fichasColocar){
+        boolean retornar = false;
+        if (!grupoModificar.getListaFichas().isEmpty()) {
+            for (int i = 0; i < grupoModificar.getListaFichas().size(); i++) {
+//                if (grupoModificar.getListaFichas().get(i).getColor()){
+//                        retornar = true;
+//                }
             }
         }
-        if (fichasRemovidas == true) {
-            agregarGrupo(grupo);
-        }else{
-            throw new IllegalStateException("No se encontraron las fichas");
-        }
-
+        return retornar;
     }
 
     /**
@@ -90,6 +137,18 @@ public class Grupo {
             return true;
         }else{
             return false;
+        }
+    }
+
+    /**
+     * Método para agregar las fichas al grupo
+     * @param fichasAgregar
+     */
+    public void agregarFichas(List<Ficha> fichasAgregar){
+        if (!fichasAgregar.isEmpty()) {
+            for (int i = 0; i < fichasAgregar.size(); i++) {
+                listaFichas.add(fichasAgregar.get(i));
+            }
         }
     }
 }
