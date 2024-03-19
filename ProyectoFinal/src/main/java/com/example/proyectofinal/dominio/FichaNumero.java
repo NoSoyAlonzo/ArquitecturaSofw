dc0bfb90d5bd2e0f5e81d6ab2966904dd38cfb05
ArturoLuna7 <arturo712luna@gmail.com>
2024-03-18 22:19:45 -0700

    comentarios

--- ProyectoFinal/src/main/java/com/example/proyectofinal/dominio/FichaNumero.java
+++ ProyectoFinal/src/main/java/com/example/proyectofinal/dominio/FichaNumero.java
@@ -1,13 +1,25 @@
 package com.example.proyectofinal.dominio;
 
 public class FichaNumero extends Ficha{
+    /**
+     * Atributos de la clase
+     */
     private int numero;
 
+    /**
+     * Constructor con parámetros
+     * @param color
+     * @param numero
+     */
     public FichaNumero(ColorManager color, int numero) {
         super(color);
         this.numero = numero;
     }
 
+    /**
+     * Métodos get y set
+     * @return
+     */
     public int getNumero() {
         return numero;
     }
