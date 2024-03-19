def3506fda6feca8859b493f1823612c7654ada9
ArturoLuna7 <arturo712luna@gmail.com>
2024-03-18 22:13:47 -0700

    Se agregaron comentarios y métodos

--- ProyectoFinal/src/main/java/com/example/proyectofinal/modelo/ModeloJuego.java
+++ ProyectoFinal/src/main/java/com/example/proyectofinal/modelo/ModeloJuego.java
@@ -2,21 +2,35 @@ package com.example.proyectofinal.modelo;
 
 import com.example.proyectofinal.dominio.*;
 
+import java.util.List;
+
 
 public class ModeloJuego implements ModeloFiltro {
 
+    /**
+     * Atributos de la clase ModeloJuego
+     */
     private static ModeloJuego instanciaModeloJuego;
     private Pozo pozo;
     private Tablero tablero;
     private Juego juego;
     private Observador observador;
 
+    /**
+     * Método constructor de un ModeloJuego
+     * privado para aplicar el patrón singleton
+     */
     private ModeloJuego(){
         pozo = Pozo.getInstance();
         tablero = Tablero.getInstance();
         juego = Juego.getInstance();
     }
 
+    /**
+     * Método que permite obtener una instancia de
+     * la clase ModeloJuego
+     * @return
+     */
     public static synchronized ModeloJuego getInstance(){
         if (instanciaModeloJuego == null){
             instanciaModeloJuego = new ModeloJuego();
@@ -24,27 +38,51 @@ public class ModeloJuego implements ModeloFiltro {
         return instanciaModeloJuego;
     }
 
-    //metodos
+    /**
+     * Método que le habla al dividir grupo del tablero.
+     * @param grupo
+     */
     public void dividirGrupo(Grupo grupo){
         tablero.obtenerGrupoDividido(grupo);
     }
 
-    public void verifColocarFichaGrupo(Grupo grupo){
-        tablero.obtenerGrupoDividido(grupo);
+    /**
+     * Método que le habla al método verificarColocarFicha
+     * de la clase grupo
+     * @param grupo
+     */
+    public void verifColocarFichaGrupo(Grupo grupo, List<Ficha> fichasColocar){
+        tablero.verificarColocarFicha(grupo, fichasColocar, juego);
     }
 
+    /**
+     * Método que le habla al método
+     * obtenerGruposModificados de la clase Tablero.
+     */
     public void validarGrupos(){
         tablero.obtenerGruposModificados();
     }
 
+    /**
+     * Método que le habla al método jalarFichaPozo
+     * de la clase Juego
+     */
     public void jalarFichaPozo(){
         juego.jalarFichaPozo();
     }
 
+    /**
+     * Método que le habla al método
+     * obtenerTurnoSiguiente de la clase Juego
+     */
     public void pasarTurno(){
         juego.obtenerTurnoSiguiente();
     }
 
+    /**
+     * Método que permitirá actualizar los datos del negocio
+     * según haya cambiado la pantalla
+     */
     @Override
     public void actualizar() {
 
