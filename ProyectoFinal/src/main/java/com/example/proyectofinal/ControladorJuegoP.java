b0b79c2953c5b5c180b6520ab963a0ffb2a30bdc
NoSoyAlonzo <56053393+NoSoyAlonzo@users.noreply.github.com>
2024-05-16 22:54:54 -0600

    cambios  pantalla

--- ProyectoFinal/src/main/java/com/example/proyectofinal/ControladorJuegoP.java
+++ ProyectoFinal/src/main/java/com/example/proyectofinal/ControladorJuegoP.java
@@ -3,11 +3,14 @@ package com.example.proyectofinal;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
 import javafx.scene.control.Button;
 import javafx.scene.layout.AnchorPane;
+import javafx.stage.Stage;
 
 
-    public class ControladorJuegoP {
+public class ControladorJuegoP {
 
         @FXML
         private Button btnJugar;
@@ -18,10 +21,15 @@ import javafx.scene.layout.AnchorPane;
         @FXML
         void abrirVentana(ActionEvent event) {
             try{
-                FXMLLoader fx = new FXMLLoader(this.getClass().getResource(""))
-
-
-
+                FXMLLoader fx = new FXMLLoader(this.getClass().getResource("MenuPrincipalJ.fxml"));
+                Parent root1=(Parent) fx.load();
+                Stage stage= new Stage();
+                stage.setTitle("");
+                stage.setScene(new Scene(root1));
+                stage.show();
+            }
+            catch(Exception e){
+                System.out.println("No se pudo abrir");
             }
         }
 }
